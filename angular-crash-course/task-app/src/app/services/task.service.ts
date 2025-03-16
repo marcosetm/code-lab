d8995c26d46fa9df91ac47b8609a374a53df91de
marcosetm <m2e5090@gmail.com>
2025-03-16 14:37:25 -0700

    setup deleteTask function

--- angular-crash-course/task-app/src/app/services/task.service.ts
+++ angular-crash-course/task-app/src/app/services/task.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Task } from '../Task';
-import { Observable } from 'rxjs';
+import { catchError, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -14,6 +14,10 @@ export class TaskService {
 
   getTasks(): Observable<Task[]> {
     return this.http.get<Task[]>(this.url);
+  }
+
+  deleteTask(id: Number): Observable<any> {
+    return this.http.delete<Task>(`${this.url}/${id}`);
   }
   
 }
