d98870548237b5f3bb3550d35edb2a216a15eeeb
marcosetm <m2e5090@gmail.com>
2025-03-17 20:01:06 -0700

    import FormsModule and setup two-way data binding"

--- angular-crash-course/task-app/src/app/components/add-task/add-task.component.ts
+++ angular-crash-course/task-app/src/app/components/add-task/add-task.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-add-task',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './add-task.component.html',
   styleUrl: './add-task.component.css'
 })
 export class AddTaskComponent {
+  text!: string;
+  day!: string;
+  reminder: boolean = false;
 
 }
