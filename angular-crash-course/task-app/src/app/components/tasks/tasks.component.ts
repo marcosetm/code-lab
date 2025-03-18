6f1ed7ba2adc122b8d3f3ee12b9148fca9071392
marcosetm <m2e5090@gmail.com>
2025-03-17 19:45:12 -0700

    generate add-taks comp and import into tasks comp

--- angular-crash-course/task-app/src/app/components/tasks/tasks.component.ts
+++ angular-crash-course/task-app/src/app/components/tasks/tasks.component.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { TaskService } from '../../services/task.service';
 import { Task } from '../../Task';
 import { TaskItemComponent } from '../task-item/task-item.component';
+import { AddTaskComponent } from '../add-task/add-task.component';
 
 @Component({
   selector: 'app-tasks',
-  imports: [CommonModule, TaskItemComponent],
+  imports: [CommonModule, TaskItemComponent, AddTaskComponent],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css'
 })
