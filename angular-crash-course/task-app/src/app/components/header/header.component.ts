20bde0b631cad572083a4897c39621445956b33c
marcosetm <m2e5090@gmail.com>
2025-03-18 21:58:10 -0700

    add router to button

--- angular-crash-course/task-app/src/app/components/header/header.component.ts
+++ angular-crash-course/task-app/src/app/components/header/header.component.ts
@@ -2,10 +2,12 @@ import { Component } from '@angular/core';
 import { ButtonComponent } from '../button/button.component';
 import { UiService } from '../../services/ui.service';
 import { Subscription } from 'rxjs';
+import { Router } from '@angular/router';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-header',
-  imports: [ButtonComponent],
+  imports: [ButtonComponent, NgIf],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
@@ -14,11 +16,15 @@ export class HeaderComponent {
   showAddTask!: boolean;
   subscription!: Subscription;
 
-  constructor (private uiService:UiService) {
+  constructor (private uiService: UiService, private router: Router) {
     this.subscription = this.uiService.onToggle().subscribe((value) => (this.showAddTask = value));
   }
 
   toggleAddTask() {
     this.uiService.toggleAddTask();
+  }
+
+  hasRoute(route: string) {
+    return this.router.url === route;
   }
 }
