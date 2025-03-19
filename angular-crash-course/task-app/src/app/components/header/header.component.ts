387ce7594becddeb95d5a144ae89605219d15a3d
marcosetm <m2e5090@gmail.com>
2025-03-18 21:08:05 -0700

    wire button to service

--- angular-crash-course/task-app/src/app/components/header/header.component.ts
+++ angular-crash-course/task-app/src/app/components/header/header.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { ButtonComponent } from '../button/button.component';
+import { UiService } from '../../services/ui.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-header',
@@ -9,8 +11,14 @@ import { ButtonComponent } from '../button/button.component';
 })
 export class HeaderComponent {
   title: string = 'Task App';
+  showAddTask!: boolean;
+  subscription!: Subscription;
+
+  constructor (private uiService:UiService) {
+    this.subscription = this.uiService.onToggle().subscribe((value) => (this.showAddTask = value));
+  }
 
   toggleAddTask() {
-    console.log("toggleAddTask from header.component")
+    this.uiService.toggleAddTask();
   }
 }
--- angular-crash-course/task-app/src/app/services/ui.service.ts
+++ angular-crash-course/task-app/src/app/services/ui.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { Observable, Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UiService {
+  private showAddTaks: boolean = false;
+  private subject: Subject<any> = new Subject<any>();
 
   constructor() { }
+
+  toggleAddTask(): void {
+    this.showAddTaks = !this.showAddTaks;
+    this.subject.next(this.showAddTaks);
+  }
+
+  onToggle(): Observable<any> {
+    return this.subject.asObservable();
+  }
 }
