50c6734c65be9f2883b8db707bbb6512dfccdd2c
marcosetm <m2e5090@gmail.com>
2025-03-18 21:40:22 -0700

    setup and add about route and page

--- angular-crash-course/task-app/src/app/app.component.ts
+++ angular-crash-course/task-app/src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { TasksComponent } from './components/tasks/tasks.component';
+import { AboutComponent } from './components/about/about.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, TasksComponent],
+  imports: [RouterOutlet, HeaderComponent, TasksComponent, AboutComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
