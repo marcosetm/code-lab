672598eb4ae87ddee685df0252f559dbb458b001
marcosetm <m2e5090@gmail.com>
2025-04-15 22:04:00 -0700

    setup IntelliJ for devtools

--- dev-spring-boot/01-spring-boot-overview/02-dev-tools-demo/src/main/java/com/springweb/springwebapp/rest/FunRestController.java
+++ dev-spring-boot/01-spring-boot-overview/02-dev-tools-demo/src/main/java/com/springweb/springwebapp/rest/FunRestController.java
@@ -6,9 +6,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class FunRestController {
 
-    // expose '/' and return 'Hello World'
+    // expose endpoint '/' and return 'Hello World'
     @GetMapping("/")
     public String hello() {
         return "Hello World";
+    }
+
+    // expose endpoint "workout"
+    @GetMapping("workout")
+    public String getWorkout() {
+        return "Running workout";
+    }
+
+    // expose endpoint "fortune"
+    @GetMapping("fortune")
+    public String getFortune() {
+        return "Making it rain.";
     }
 }
