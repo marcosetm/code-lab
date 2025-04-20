24ccfee027cdf243b1e484d73fb8e65d375b042e
marcosetm <m2e5090@gmail.com>
2025-04-20 14:48:11 -0700

    create project

---
+++ dev-spring-boot/03-spring-boot-hibernate-jpa-crud/hibernatecrud/src/test/java/com/spring/hibernatecrud/HibernatecrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.hibernatecrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HibernatecrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
